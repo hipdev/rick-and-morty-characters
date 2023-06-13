c4aa45602e7390166c17ddfd8dd23f0e8df9093b
Julian David <julianfullstackdev@gmail.com>
2023-06-13 17:44:53 -0500

    I'm back, check if reached end

--- components/Home/CharactersPaginationClient.tsx
+++ components/Home/CharactersPaginationClient.tsx
@@ -24,14 +24,20 @@ export default function CharactersPaginationClient() {
         })
       )}
 
-      <button
-        className="sm:cols-span-2  mt-5 md:col-span-3 2xl:col-span-4"
-        onClick={handleSize}
-      >
-        <span className="rounded-sm border border-white/70 px-4 py-2 text-white">
-          Load More
-        </span>
-      </button>
+      {data && data[data.length - 1].info.next == null ? (
+        <p className="sm:cols-span-2 mt-5 text-center text-lg font-bold text-white md:col-span-3 2xl:col-span-4">
+          You reached the end
+        </p>
+      ) : (
+        <button
+          className="sm:cols-span-2  mt-5 md:col-span-3 2xl:col-span-4"
+          onClick={handleSize}
+        >
+          <span className="rounded-sm border border-white/70 px-4 py-2 text-white">
+            Load More
+          </span>
+        </button>
+      )}
     </>
   );
 }
