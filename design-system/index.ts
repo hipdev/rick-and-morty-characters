b3a116ac999d863e207a18e3620b9c7e0c26881a
Julian David <julianfullstackdev@gmail.com>
2023-06-13 19:25:27 -0500

    finish feature, new big card element

--- design-system/index.ts
+++ design-system/index.ts
@@ -1,6 +1,7 @@
 export { Header } from "./elements/Header/Header";
 export { Footer } from "./elements/Footer/Footer";
 export { SmallCardLink } from "./elements/SmallCardLink/SmallCardLink";
+export { BigCard } from "./elements/BigCard/BigCard";
 export { Status } from "./elements/Status/Status";
 
 // UI
